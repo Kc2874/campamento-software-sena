0dd56765ce34d896b999230a96db5ae5442ade1d
Your Name <you@example.com>
2022-09-15 11:30:51 -0500

    Bootcamp Controller

--- backend/app/Http/Controllers/bootcampController.php
+++ backend/app/Http/Controllers/bootcampController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Bootcamp;
 
 class bootcampController extends Controller
 {
@@ -13,7 +14,7 @@ class bootcampController extends Controller
      */
     public function index()
     {
-        echo"aqui se van a mostrar todos los bootcamps";
+       return response()->json(["success"=>true,"data"=>Bootcamp::all()],200);
     }
 
     /**
@@ -24,7 +25,9 @@ class bootcampController extends Controller
      */
     public function store(Request $request)
     {
-        echo"aqui se va a guardar un nuevo bootcamp";
+       //verificar los datos del payload
+       //guardar el nuevo bootcamp
+      return response()->json(["success"=>true,"data"=>Bootcamp::create($request->all())],201);
     }
 
     /**
@@ -35,7 +38,7 @@ class bootcampController extends Controller
      */
     public function show($id)
     {
-        echo"aqui se va a mostrar el bootcamp cuyo Id es $id";
+        return response()->json(["success"=>true,"data"=>Bootcamp::find($id)],200);
     }
 
     /**
@@ -47,7 +50,14 @@ class bootcampController extends Controller
      */
     public function update(Request $request, $id)
     {
-        echo"aqui se va actualizar un bootcamp cuyo id es $id";
+       //1. Seleccionar el bootcamp a actualizar
+       $b = Bootcamp::find($id);
+       //2.Actualizar el bootcamp
+       $b->update($request->all());
+       //3.Enviar el bootcamp actualizado
+       return response()->json([
+        "success"=>true,
+        "data"=>$b],200);
     }
 
     /**
@@ -58,6 +68,8 @@ class bootcampController extends Controller
      */
     public function destroy($id)
     {
-        echo"aqui se va a elminar el bootcamp cuyo id es $id";
+        $b= Bootcamp::find($id);
+        $b->delete();
+        return response()->json(["success"=>true,"data"=>$b]);
     }
 }
